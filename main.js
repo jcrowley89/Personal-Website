999c4e637fe9097b4c224c1b16187d6f4229ae3c
John Crowley <jcrowley89yahoo.com>
2021-05-27 16:08:16 -0400

    added autoscroll function, other minor edits

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
 const jumbotron = document.querySelector("header.jumbotron");
-const navbar = document.querySelector("div.navbar")
-const menuButton = document.querySelector("#menuButton")
-const menuButtonIcon = document.querySelector(".bi-text-right")
-const siteNav = document.querySelector("#siteNav")
-const icons = document.querySelectorAll("i")
+const navbar = document.querySelector("div.navbar");
+const menuButton = document.querySelector("#menuButton");
+const menuButtonIcon = document.querySelector(".bi-text-right");
+const siteNav = document.querySelector("#siteNav");
+const navLinks = document.querySelectorAll(".nav-link");
+const icons = document.querySelectorAll("i");
 let scrollPos = window.scrollY;
 
 window.addEventListener("scroll", function () {
   scrollPos = window.scrollY;
   if (scrollPos >= 1) {
-    navbar.classList.add("fade-in", "bg-white", "shadow")
+    navbar.classList.add("fade-in", "bg-white", "shadow");
   } else {
-    navbar.classList.remove("fade-in", "bg-white", "shadow")
+    navbar.classList.remove("fade-in", "bg-white", "shadow");
   }
 });
 
+function showNav() {
+  siteNav.classList.remove("hidden");
+  menuButtonIcon.className = "bi-x";
+  icons.forEach((icon) => icon.classList.add("text-white"));
+}
+function hideNav() {
+  siteNav.classList.add("hidden");
+  menuButtonIcon.className = "bi-text-right";
+  icons.forEach((icon) => icon.classList.remove("text-white"));
+}
 menuButton.addEventListener("click", function () {
   if (menuButtonIcon.className === "bi-text-right") {
-    siteNav.classList.remove("hidden")
-    menuButtonIcon.className = "bi-x"
-    icons.forEach(icon => icon.classList.add("text-white"))
+    showNav();
   } else {
-    siteNav.classList.add("hidden")
-    menuButtonIcon.className = "bi-text-right"
-    icons.forEach(icon => icon.classList.remove("text-white"))
+    hideNav();
   }
+});
+navLinks.forEach((link) => {
+  link.addEventListener("click", hideNav)
 })
